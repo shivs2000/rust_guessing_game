1e292c53ac5fafef16a9cd3f192e60c7c0a55650
SHIVAM@!! <43001208+shivs2000@users.noreply.github.com>
2022-10-13 15:59:09 +0530

    added intial code

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use std::io;
+
+fn main() {
+   println!("Guess the number");
+   println!("Enter the number");
+let mut guess=String::new();
+
+   io::stdin()
+   .read_line(&mut guess)
+   .expect("failed to load lines");
+   
+}
